1dddc4eaf79f96237e4d3911702caffffcbef2cf
qycelo <96103642+qycelo@users.noreply.github.com>
2022-07-10 15:06:38 +0300

    Create main.py

---
+++ Python/Moderation[cogs]/main.py
@@ -0,0 +1,34 @@
+# Imports
+import nextcord
+from nextcord.ext import commands
+
+# Config #
+# Embed
+footer = "Embed footer | qycelo#2202"
+color = 0x03fc41 # green
+wcolor = 0xfc0303 # red
+# Command admin
+admin = ["Discord_id", "Discord_id"]
+
+# Token
+# Discord developer portal
+TOKEN = ""
+
+# Intents, prefix, remove help
+# Discord devreloper portal trun on intents
+intents = nextcord.Intents.default()
+qyc = commands.Bot(command_prefix=";", intents=nextcord.Intents.all())
+qyc.remove_command("help")
+
+# On ready, status
+# Playing, listening, streaming
+@qyc.event
+async def on_ready():
+    await qyc.change_presence(status=nextcord.Status.online,
+    activity=nextcord.Activity(
+        type=nextcord.ActivityType.listening,
+        name="qycelo#2202 "))
+
+    print(f"{qyc.user} is online!")
+
+qyc.run(TOKEN)
